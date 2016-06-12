2290c74497f9aeeaebf7a4621cabb2fbd726bb6d
Dan Sheffner <Dan@Sheffner.com>
2016-06-12 08:35:16 -0500

    generic tmux files for docker container

---
+++ vim/tmux_virtical2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright (c) 2016, Dan Sheffner Digital Imaging Software Soltuions, INC
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+# creates a 2 horizontal terminal
+
+tmux start-server
+
+tmux new-session -d -s sessionA -n screen1
+tmux split-window -h -p 50
+tmux select-pane -t sessionA.0
+tmux attach-session -t sessionA
