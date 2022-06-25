bb8a2a086e7b38390d6d1ec69229f932df7c64d8
Dan Sheffner <Dan@Sheffner.com>
2022-06-24 22:33:56 -0500

    adding support for vscode container

---
+++ vscode_bootstrap/boot.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# this will bootstrap a debian based docker container
+clear
+echo "dockervscode_bootstrap.sh started..."
+
+# apt-get
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -yq \
+    curl \
+    wget \
+    git \
+    python3-pip \
+    python3-dev \
+    build-essential \
+    gcc \
+    vim \
+    tmux \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+	libreadline-dev \
+	libsqlite3-dev \
+	llvm \
+	libncurses5-dev \
+	libncursesw5-dev \
+	xz-utils tk-dev \
+	libffi-dev \
+	liblzma-dev
+
+# this is a crude check to see if the pyenv already exists.
+# if does (2nd run) it will skip this putting it in the .bashrc
+FILE1=/home/vscode/.pyenv/libexec/pyenv
+if [ ! -f $FILE1 ]
+	cat pyenvbash >> ~/.bashrc
+fi
+
+FILE2=/home/vscode/.pyenv/versions/3.11.0b3/bin/python3
+if [ ! -f $FILE2 ]
+	curl -Lq https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+	source ~/.bashrc
+
+	# install beta version of python3.11 for speed
+	pyenv install 3.11.0b3
+
+	# create a good symlink
+	sudo ln -s /home/vscode/.pyenv/versions/3.11.0b3/bin/python3 /usr/local/bin/python3.11
+else
+	echo "skipping install of python3.11.  Found a binary..."
+
+echo "dockervscode_bootstrap.sh completed."
