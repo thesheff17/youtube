820a15b623b3f3fcfad4508ffe9e4fe655ef4405
Dan Sheffner <thesheff17@gmail.com>
2023-07-29 20:39:13 -0500

    cleanup of some code and for loop

--- streams/2023-06-29-captnproto-pycapnp/captproto_reader.py
+++ streams/2023-06-29-captnproto-pycapnp/captproto_reader.py
@@ -9,15 +9,10 @@ def readStuff():
     awsdata_1 = awsdata_capnp.AwsdataA.read(f1)
     f1.close()
 
-    # my_dict = {'ec2id': 'id-bladslfksdfl'}
-    # v1 = awsdata_capnp.AwsdataA.new_message(**my_dict)
-    # running = awsdata_1.new_message(**my_dict)
-    # print (v1)
-
     for a1 in awsdata_1.awsdata3:
         print (a1.to_dict())
-        # print (a1.ec2id)
-        # print (a1.status)
+        print (a1.ec2id)
+        print (a1.status)
 
 if __name__ == "__main__":
     readStuff()
